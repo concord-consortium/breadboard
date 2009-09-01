bf8d28265e95e0ab63962d17fc77a65080252bc4
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2009-09-01 21:17:12 +0000

    added a start button, and added start and stop times to each sub question. no serialization yet.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/sparks/sparks-content@20117 6e01202a-0783-4428-890a-84243c50cc2b

--- demos/resistor-colors/sparks.js
+++ demos/resistor-colors/sparks.js
@@ -10,17 +10,26 @@ function enableForm(form){
   form.find("button").click(buttonClicked) 
   form.find("input, select").removeAttr("disabled")
   form.css("background-color", "rgb(253,255,184)")
+  form.find("input[name='start_time']").text("" + (new Date()).getTime())
 }
 
 function disableForm(form){
+  form.find("input[name='stop_time']").text("" + (new Date()).getTime())
   form.find("button").remove() 
   form.find("input, select").attr("disabled", "true")
   form.css("background-color", "")
 }
 
 $(document).ready(function(){
+   // disable all form elements
    $("input, select").attr("disabled", "true")
 
-   form = $("form:first")
-   enableForm(form)
+   // add start and stop times to all forms
+   $("form").append(
+     "<input name='start_time' type='hidden'></input><input name='stop_time' type='hidden'></input>")   
+
+   button = $("button[name='start']").click(function(event){
+     form = $("form:first")
+     enableForm(form)     
+   })
  });
