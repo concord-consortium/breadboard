c3e474139225eea8bd0a63b01a79ef9dce5cb0cf
Seong-June Kim <jkim707@gmail.com>
2010-04-11 23:14:02 -0400

    Bugfix for rounding error for getColors
    Alistair's fix

--- common/javascript/circuit/resistor-5band.js
+++ common/javascript/circuit/resistor-5band.js
@@ -8,6 +8,12 @@ var Resistor5band = function() {
 
 Util.extend(Resistor5band, Resistor, {
   
+    colMap: ['black', 'brown', 'red', 'orange', 'yellow',
+             'green','blue','violet','grey','white',
+             'gold','silver'],
+    
+    tolMap: { _01:'brown', _02:'red', _05:'grey', _1:'violet', _25:'blue', _5:'green' },
+    
     randomize : function() {
       var ix = this.randInt(0, 1);
       var values;
@@ -28,7 +34,7 @@ Util.extend(Resistor5band, Resistor, {
     },
     
     // rvalue: resistance value
-    getColors : function(rvalue, tolerance) {
+    getColors_old : function(rvalue, tolerance) {
         for (var exp = 0; exp < 9; ++exp) {
             if (Math.floor(rvalue / Math.pow(10, exp)) === 0) {
                 break;
@@ -41,6 +47,28 @@ Util.extend(Resistor5band, Resistor, {
         var sig3 = Math.floor(tempValue / Math.pow(10, exp-3));
         return [this.colorMap[sig1], this.colorMap[sig2], this.colorMap[sig3],
                 this.colorMap[exp-3], this.toleranceColorMap[tolerance]];
-    }
+    },
     
+    getColors: function(ohms, tolerance) {
+        var ohmsStr = '' + ohms;
+        var sigfigs = '';
+        var dec = false;
+        var pwr = 0;
+        var iohms = '';
+        
+        for (var i = 0; i < ohmsStr.length; ++i) {
+          iohms = ohmsStr[i];
+          
+          switch(iohms)
+          {
+          case '.': dec = true; break;
+          case '0': pwr += !dec && sigfigs.length > 1 ? 1 : 0;
+          default: sigfigs += iohms; break;
+          }
+        }
+        var mult = ohmsStr.indexOf('.') > -1 ? 9 + ohmsStr.split('.')[1].length : pwr;
+        return [ this.colMap[sigfigs[0]], this.colMap[sigfigs[1]],
+                 this.colMap[sigfigs[2]], this.colMap[mult],
+                 this.tolMap['_' + ('' + tolerance).split(".")[1]] ];
+    }
 });
