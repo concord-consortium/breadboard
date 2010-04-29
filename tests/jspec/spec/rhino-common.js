3f6052acf396ae093929befdaf85f2d35a1ac684
Seong-June Kim <jkim707@gmail.com>
2010-04-29 15:56:30 -0400

    Update load files to make jspec test work

--- tests/jspec/spec/rhino-common.js
+++ tests/jspec/spec/rhino-common.js
@@ -1,7 +1,7 @@
 load('spec/support/env.rhino.js');
 
-load('../../lib/jquery/jquery-1.3.2.js');
-load('../../lib/jquery/jquery.url.packed.js');
+load('../../lib/jquery/jquery-1.4.2.min.js');
+load('../../lib/jquery/plugins/jquery.url.packed.js');
 load('../../lib/json2.js');
 
 load('spec/lib/jspec.js');
@@ -11,11 +11,8 @@ load('spec/lib/jspec.xhr.js');
 load('spec/lib/jspec.shell.js');
 load('spec/support/junit.xml.js');
 
-load('../../activities/measuring-resistance/javascript/assessment/feedback.js');
-load('../../activities/measuring-resistance/javascript/assessment/grader.js');
-load('../../activities/measuring-resistance/javascript/assessment/log_parser.js');
-load('../../common/javascript/util/util.js');
 load('../../common/javascript/sparks-init.js');
+load('../../common/javascript/util/util.js');
 load('../../common/javascript/flash_comm.js');
 load('../../common/javascript/circuit/r-values.js');
 load('../../common/javascript/circuit/resistor.js');
@@ -24,6 +21,13 @@ load('../../common/javascript/circuit/resistor-5band.js');
 load('../../common/javascript/circuit/multimeter.js');
 load('../../common/javascript/util/mymath.js');
 load('../../common/javascript/util/unit.js');
+load('../../common/javascript/activity.js');
+
+load('../../activities/measuring-resistance/javascript/activity.js');
+load('../../activities/measuring-resistance/javascript/assessment/feedback.js');
+load('../../activities/measuring-resistance/javascript/assessment/grader.js');
+load('../../activities/measuring-resistance/javascript/assessment/log_parser.js');
+
 load('lib/common.js');
 
 specResults = JSpec
