6b59cc4e0c8c04120ed697be8359435ce51779bc
Seong-June Kim <jkim707@gmail.com>
2010-04-29 11:47:26 -0400

    Add "close" and "tutorial" button to popup feedback

--- activities/measuring-resistance/javascript/assessment/reporter.js
+++ activities/measuring-resistance/javascript/assessment/reporter.js
@@ -7,6 +7,10 @@ function Reporter(assessment)
 
 Reporter.prototype =
 {
+    readingHintPath: sparks.config.root_dir + '/common/resources/hint1_colorcode.html',
+    measuringHintPath: sparks.config.root_dir + '/common/resources/MeasureResMovie/index.html',
+    toleranceHintPath: sparks.config.root_dir + '/common/resources/hint1_calctolerance.html',
+    
     red : '#cc3300',
     red2 : '#cc9933',
     orange : '#ff6600',
@@ -69,37 +73,37 @@ Reporter.prototype =
         text = questions[0].answer ? questions[0].answer + questions[0].unit : 'No Answer';
         this.setAnswerTextWithColor('#rated_r_answer', text, fb);
         $('#rated_r_points').text(fb.getPoints());
-        this.addFeedback($('#rated_r_feedback'), fb);
+        this.addFeedback($('#rated_r_feedback'), fb, this.readingHintPath);
         
         fb = feedback.root.reading.rated_t_value;
         $('#rated_t_correct').text(questions[1].correct_answer * 100 + '%');
         text = questions[1].answer ? questions[1].answer + questions[1].unit : 'No Answer';
         this.setAnswerTextWithColor('#rated_t_answer', text, fb);
         $('#rated_t_points').text(fb.getPoints());
-        this.addFeedback($('#rated_t_feedback'), fb);
+        this.addFeedback($('#rated_t_feedback'), fb, this.readingHintPath);
         
         fb = feedback.root.t_range_value;
         $('#t_range_correct').text(Unit.res_str(questions[3].correct_answer[0]) + ' .. ' + Unit.res_str(questions[3].correct_answer[1]));
         text = (questions[3].answer[0] || questions[3].answer[1]) ? String(questions[3].answer[0]) + questions[3].unit[0] + ' .. ' + questions[3].answer[1] + questions[3].unit[1] : 'No Answer';
         this.setAnswerTextWithColor('#t_range_answer', text, fb);
-        this.addFeedback($('#t_range_feedback'), fb);
+        this.addFeedback($('#t_range_feedback'), fb, this.toleranceHintPath);
 
         fb = feedback.root.within_tolerance;
         $('#within_correct').text(questions[4].correct_answer);
         text = questions[4].answer ? questions[4].answer : 'No Answer';
         this.setAnswerTextWithColor('#within_answer', text, fb);
         $('#within_points').text(fb.getPoints());
-        this.addFeedback($('#within_feedback'), fb);
+        this.addFeedback($('#within_feedback'), fb, this.toleranceHintPath);
 
         fb = feedback.root.time.reading_time;
         this.setAnswerTextWithColor('#reading_time', sparks.util.timeLapseStr(questions[0].start_time, questions[1].end_time), fb);
         $('#reading_time_points').text(fb.getPoints());
-        this.addFeedback($('#reading_time_feedback'), fb);
+        this.addFeedback($('#reading_time_feedback'), fb, this.readingHintPath);
 
         fb = feedback.root.time.measuring_time;
         this.setAnswerTextWithColor('#measuring_time', sparks.util.timeLapseStr(questions[2].start_time, questions[2].end_time), fb);
         $('#measuring_time_points').text(fb.getPoints());
-        this.addFeedback($('#measuring_time_feedback'), fb);
+        this.addFeedback($('#measuring_time_feedback'), fb, this.measuringHintPath);
         
         fb = feedback.root.measuring.probe_connection;
         if (fb.correct == 4) {
@@ -150,7 +154,7 @@ Reporter.prototype =
         this.setTextWithColor('#knob_setting_answer', this.dialLabels[feedback.submit_dial_setting], color);
         
         $('#knob_setting_points').text(fb.getPoints());
-        this.addFeedback($('#knob_setting_feedback'), fb);
+        this.addFeedback($('#knob_setting_feedback'), fb, this.measuringHintPath);
         
         if (feedback.root.measuring.power_switch.correct == 4) {
         	this.setTextWithColor('#power_switch', 'On', this.green);
@@ -167,7 +171,7 @@ Reporter.prototype =
         text = questions[2].answer ? questions[2].answer + questions[2].unit : 'No Answer';
         this.setAnswerTextWithColor('#measured_r_answer', text, fb);
         $('#measured_r_points').text(fb.getPoints());
-        this.addFeedback($('#measured_r_feedback'), fb);
+        this.addFeedback($('#measured_r_feedback'), fb, this.measuringHintPath);
         
         fb = feedback.root.measuring.plug_connection;
         if (fb.correct == 4) {
@@ -177,7 +181,7 @@ Reporter.prototype =
             this.setTextWithColor('#plug_connection_answer', 'Incorrect', this.red);
         }
         $('#plug_connection_points').text(fb.getPoints());
-        this.addFeedback($('#plug_connection_feedback'), fb);
+        this.addFeedback($('#plug_connection_feedback'), fb, this.measuringHintPath);
 
         fb = feedback.root.measuring.probe_connection;
         if (fb.correct == 4) {
@@ -187,7 +191,7 @@ Reporter.prototype =
             this.setTextWithColor('#probe_connection_answer', 'Incorrect', this.red);
         }
         $('#probe_connection_points').text(fb.getPoints());
-        this.addFeedback($('#probe_connection_feedback'), fb);
+        this.addFeedback($('#probe_connection_feedback'), fb, this.measuringHintPath);
 
         fb = feedback.root.measuring.power_switch;
         if (fb.correct == 4) {
@@ -197,7 +201,7 @@ Reporter.prototype =
             this.setTextWithColor('#power_switch_answer', 'Incorrect', this.red);
         }
         $('#power_switch_points').text(fb.getPoints());
-        this.addFeedback($('#power_switch_feedback'), fb);
+        this.addFeedback($('#power_switch_feedback'), fb, this.measuringHintPath);
         
         fb = feedback.root.measuring.task_order;
         if (fb.correct == 4) {
@@ -207,7 +211,7 @@ Reporter.prototype =
             this.setTextWithColor('#task_order_answer', 'Incorrect', this.red);
         }
         $('#task_order_points').text(fb.getPoints());
-        this.addFeedback($('#task_order_feedback'), fb);
+        this.addFeedback($('#task_order_feedback'), fb, this.measuringHintPath);
         
         fb = feedback.root.reading;
         $('#reading_points').html('<b>' + fb.getPoints() + ' of ' +
@@ -236,24 +240,25 @@ Reporter.prototype =
         var rootDir = sparks.config.root_dir;
 
         var fb = feedback.root.reading;
+
         if (fb.getPoints() != fb.getMaxPoints()) {
             this.imageLink($('#reading_tutorial_link'),
                 rootDir + '/common/icons/tutorial.png',
-                rootDir + '/common/resources/hint1_colorcode.html');
+                this.readingHintPath);
         }
 
         fb = feedback.root.measuring;
         if (fb.getPoints() != fb.getMaxPoints()) {
             this.imageLink($('#measuring_tutorial_link'),
                 rootDir + '/common/icons/tutorial.png',
-                rootDir + '/common/resources/MeasureResMovie/index.html');
+                this.measuringHintPath);
         }
 
         fb = feedback.root.t_range_value;
         if (fb.getPoints() != fb.getMaxPoints()) {
             this.imageLink($('#t_range_tutorial_link'),
                 rootDir + '/common/icons/tutorial.png',
-                rootDir +'/common/resources/hint1_calctolerance.html');
+                this.toleranceHintPath);
         }
     },
     
@@ -284,27 +289,43 @@ Reporter.prototype =
       container.html(a);
     },
     
-    addFeedback: function (elem, fb) {
+    addFeedback: function (elem, fb, tutorialURL) {
         var fbs = fb.feedbacks;
         for (var i = 0; i < fbs.length; ++i) {
-            elem.append(this.getFeedbackLine(fbs[i]).append($('<br />')));
+            elem.append(this.getFeedbackLine(fbs[i], tutorialURL));
+            elem.append($('<br />'));
         }
     },
     
-    getFeedbackLine: function (fb) {
+    getFeedbackLine: function (fb, tutorialURL) {
         var imgPath = sparks.config.root_dir + '/common/icons/spark.png';
         var img = $('<img></img>').addClass('no_border').attr('src', imgPath);
         
         var a = $('<a></a>').attr('href', '').append(img);
         a.append(fb[0]);
         
+        var tutorialLink = $('<a>Tutorial</a>');
+        tutorialLink.attr({ href: tutorialURL, target: 'tutorial'});
+        tutorialLink.css('float', 'right');
+        var tutorialButton = tutorialLink.button().addClass('dialog_button');
+        
+        var closeButton = $('<button>Close</button>)').button().addClass('dialog_button');
+        closeButton.css('float', 'right');
         var div = $('<div></div>').html(fb[1]);
         div.attr('title', '<img src="' + imgPath + '" /> SPARKS Feedback');
+        div.append($('<p />'));
+        div.append(closeButton).append(tutorialButton);
         var dialog = div.dialog({ autoOpen: false });
         
         a.click(function (event) {
             div.dialog('open');
             event.preventDefault();
+        });
+        tutorialButton.click(function (event) {
+            div.dialog('close');
+        });
+        closeButton.click(function (event) {
+            div.dialog('close');
         });
         return a;
     }
