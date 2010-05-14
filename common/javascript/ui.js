1bffcd05394cb21da853c1c79d249b4624deea76
Seong-June Kim <jkim707@gmail.com>
2010-05-14 15:28:16 -0400

    Add OK button to the "invalid answer" dialog

--- common/javascript/ui.js
+++ common/javascript/ui.js
@@ -3,8 +3,13 @@
     this.sparks.ui = {};
     
     sparks.ui.alert = function (title, msg) {
-        var e = $('<div>' + msg + '</div>').attr('title', title);
-        e.dialog({ dialogClass: 'alert', modal: true });
+        var div = $('<div>' + msg + '</div>').attr('title', title);
+        var okButton = $('<button>OK</button>)').button().addClass('dialog_button');
+        okButton.click(function (event) {
+            div.dialog('close');
+        });
+        div.append($('<p />')).append(okButton);
+        div.dialog({ dialogClass: 'alert', modal: true });
     };
     
 })();
