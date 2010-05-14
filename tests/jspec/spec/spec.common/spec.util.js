a090a7a02be7117fa2a3155357d1b5492f3bc790
Seong-June Kim <jkim707@gmail.com>
2010-05-14 17:33:40 -0400

    Tests for sparks.unit.res_str()

--- tests/jspec/spec/spec.common/spec.util.js
+++ tests/jspec/spec/spec.common/spec.util.js
@@ -1,21 +1,31 @@
 describe 'Unit'
 
-  describe 'nomalize'
-    it 'should normalize resistance values correctly'
-      Unit.normalizeToOhms(0, Unit.labels.ohms).should.be 0
-      Unit.normalizeToOhms(0, Unit.labels.kilo_ohms).should.be 0
-      Unit.normalizeToOhms(1000, Unit.labels.ohms).should.be 1000
-      Unit.normalizeToOhms(10, Unit.labels.kilo_ohms).should.be 10000
-      Unit.normalizeToOhms(10, Unit.labels.mega_ohms).should.be 10e6
-    end
+  before
+    u = sparks.unit
+  end
+
+  it 'should normalize resistance values correctly'
+    u.normalizeToOhms(0, u.labels.ohms).should.be 0
+    u.normalizeToOhms(0, u.labels.kilo_ohms).should.be 0
+    u.normalizeToOhms(1000, u.labels.ohms).should.be 1000
+    u.normalizeToOhms(10, u.labels.kilo_ohms).should.be 10000
+    u.normalizeToOhms(10, u.labels.mega_ohms).should.be 10e6
+  end
+
+  it 'should determine compatibility of units'
+    u.ohmCompatible(u.labels.ohms).should.be true
+    u.ohmCompatible(u.labels.kilo_ohms).should.be true
+    u.ohmCompatible(u.labels.mega_ohms).should.be true
   end
   
-  describe 'compatibility'
-    it 'should determine compatibility of units'
-      Unit.ohmCompatible(Unit.labels.ohms).should.be true
-      Unit.ohmCompatible(Unit.labels.kilo_ohms).should.be true
-      Unit.ohmCompatible(Unit.labels.mega_ohms).should.be true
-    end
+  it 'should run res_str correctly'
+    u.res_str(20).should.be '20 ' + u.labels.ohms
+    u.res_str(200).should.be '200 ' + u.labels.ohms
+    u.res_str(2000).should.be '2 ' + u.labels.kilo_ohms
+    u.res_str(200e3).should.be '200 ' + u.labels.kilo_ohms
+    
+    u.res_str(26.8519999999).should.be '26.852 ' + u.labels.ohms
+    u.res_str(0.0008000000004).should.be '0.0008 ' + u.labels.ohms
   end
 
 end
