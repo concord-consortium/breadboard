db35c4bad234fbe998d2cbc77cad539b548c449e
Seong-June Kim <jkim707@gmail.com>
2010-05-15 18:47:57 -0400

    Path correction

--- activities/measuring-resistance/javascript/assessment/learner-session-report.js
+++ activities/measuring-resistance/javascript/assessment/learner-session-report.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var mr = sparks.activities.mr;
     
     var reportId = sparks.util.readCookie('report_id');
-    var ds = new RestDS(null, null, '/sparks_report/get_report/' + reportId);
+    var ds = new RestDS(null, null, '/sparks/report/get_report/' + reportId);
     ds.readKey = true;
     ds.load(this, function(data) {
         var grader = new sparks.activities.mr.Grader(data[0]);
