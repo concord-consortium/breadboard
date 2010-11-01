84f3966e6204b88651826c4a4a4eccfbb21b0b27
Sam Fentress <sfentress@dhcp135.concord.org>
2010-11-01 12:40:32 -0400

    Add example of single html file loading different activities through url hash. WIP: refactoring folder structure.

---
+++ activities/module-2/series-measuring/activities/series-interpretive.js
@@ -0,0 +1,97 @@
+sparks.jsonActivity = {
+  "title": "Interpreting a Series Circuit",
+  "show_multimeter": "false",
+  "circuit": [
+      {
+        "type": "resistor",
+        "UID": "r1",
+        "connections": "b23,b17",
+        "label": "R1"
+      },
+      {
+        "type": "resistor",
+        "UID": "r2",
+        "connections": "c17,c11",
+        "label": "R2"
+      },
+      {
+        "type": "resistor",
+        "UID": "r3",
+        "connections": "d11,d5",
+        "label": "R3"
+      },
+      {
+        "type": "wire",
+        "connections": "left_positive20,a23"
+      },
+      {
+        "type": "wire",
+        "connections": "left_negative3,a5"
+      }
+   ],
+  "questions": [
+    {
+      "prompt": "What is the rated resistance of",
+      "subquestions": [
+        {
+          "prompt": "R<sub>1</sub>:",
+          "shortPrompt": "Resistance of R1",
+          "correct_answer": "${r1.nominalResistance}",
+          "correct_units": "ohms"
+        },
+        {
+          "prompt": "R<sub>2</sub>:",
+          "shortPrompt": "Resistance of R2",
+          "correct_answer": "${r2.nominalResistance}",
+          "correct_units": "ohms"
+        },
+        {
+          "prompt": "R<sub>3</sub>:",
+          "shortPrompt": "Resistance of R3",
+          "correct_answer": "${r3.nominalResistance}",
+          "correct_units": "ohms"
+        }
+      ]
+    },
+    {
+      "prompt": "What is the total rated resistance across all the resistors? ",
+      "shortPrompt": "Total resistance",
+      "correct_answer": "${r1.nominalResistance} + ${r2.nominalResistance} + ${r3.nominalResistance}",
+      "correct_units": "ohms"
+    },
+    {
+      "prompt": "Given that the battery is producing 9 Volts, what is the voltage drop across",
+      "subquestions": [
+        {
+          "prompt": "R<sub>1</sub>:",
+          "shortPrompt": "Voltage across R1",
+          "correct_answer": " 9 * ${r1.nominalResistance} / (${r1.nominalResistance} + ${r2.nominalResistance} + ${r3.nominalResistance})",
+          "correct_units": "V"
+        },
+        {
+          "prompt": "R<sub>2</sub>:",
+          "shortPrompt": "Voltage across R2",
+          "correct_answer": " 9 * ${r2.nominalResistance} / (${r1.nominalResistance} + ${r2.nominalResistance} + ${r3.nominalResistance})",
+          "correct_units": "V"
+        }
+      ]
+    },
+    {
+      "prompt": "What is the current through",
+      "subquestions": [
+        {
+          "prompt": "R<sub>1</sub>:",
+          "shortPrompt": "Current through R1",
+          "correct_answer": " 9 / (${r1.nominalResistance} + ${r2.nominalResistance} + ${r3.nominalResistance})",
+          "correct_units": "A"
+        },
+        {
+          "prompt": "R<sub>2</sub>:",
+          "shortPrompt": "Current through R2",
+          "correct_answer": " 9 / (${r1.nominalResistance} + ${r2.nominalResistance} + ${r3.nominalResistance})",
+          "correct_units": "A"
+        }
+      ]
+    }
+  ]
+};
