60c2e765dd6d94c2f59297721ac0f1794faea03c
Ethan McElroy <emcelroy@dhcp242.concord.org>
2012-05-21 10:49:01 -0400

    Added event tracking for additional GA account

--- app/javascript/helpers/ga-helper.js
+++ app/javascript/helpers/ga-helper.js
@@ -19,10 +19,21 @@ sparks.GAHelper.setUserLoggedIn = function (isLoggedIn) {
     userType,                       // Sets the value of "User Type" to "Member" or "Visitor" depending on status.  Required parameter.
     2                               // Sets the scope to session-level.  Optional parameter.
    ]);
+  _gaq.push(['b._setCustomVar',
+    sparks.GAHelper.USER_TYPE,      // This custom var is set to slot #1.  Required parameter.
+    'User Type',                    // The name of the custom variable.  Required parameter.
+    userType,                       // Sets the value of "User Type" to "Member" or "Visitor" depending on status.  Required parameter.
+    2                               // Sets the scope to session-level.  Optional parameter.
+   ]);
 };
 
 sparks.GAHelper.userStartedLevel = function (levelName) {
    _gaq.push(['_trackEvent',
+      sparks.GAHelper.Category.NAVIGATION, // category of activity
+      'Started new activity', // Action
+      levelName,
+   ]);
+   _gaq.push(['b._trackEvent',
       sparks.GAHelper.Category.NAVIGATION, // category of activity
       'Started new activity', // Action
       levelName,
@@ -35,10 +46,20 @@ sparks.GAHelper.userRepeatedLevel = function (levelName) {
       'Repeated activity', // Action
       levelName,
    ]);
+   _gaq.push(['b._trackEvent',
+      sparks.GAHelper.Category.NAVIGATION, // category of activity
+      'Repeated activity', // Action
+      levelName,
+   ]);
 };
 
 sparks.GAHelper.userVisitedTutorial = function (tutorialId) {
    _gaq.push(['_trackEvent',
+      sparks.GAHelper.Category.TUTORIAL, // category of activity
+      'Visited tutorial', // Action
+      tutorialId,
+   ]);
+   _gaq.push(['b._trackEvent',
       sparks.GAHelper.Category.TUTORIAL, // category of activity
       'Visited tutorial', // Action
       tutorialId,
