3b47918d22b3e0c95c7df107789697cc5e3c3880
Sam Fentress <sfentress@concord.org>
2012-09-04 14:04:08 -0400

    update client-breadboard-activity

--- client-breadboard-activity.js
+++ client-breadboard-activity.js
@@ -6103,7 +6103,7 @@ sparks.createQuestionsCSV = function(data) {
 
     showTutorial: function(filename) {
       var url = this._getURL(filename);
-      this.tutorialWindow = window.open(url,'','menubar=no,height=600,width=800,resizable=yes,toolbar=no,location=no,status=no');
+      this.tutorialWindow = window.open(url,'','menubar=no,height=600,width=800,resizable=yes,toolbar=no,location=no,status=no,scrollbars=yes');
       if (this.tutorialWindow) {
         this.tutorialWindow.moveActionCallback = this.tutorialMoveActionCallback;
       }
