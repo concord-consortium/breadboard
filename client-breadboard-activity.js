ff63767c32b0e6b906b5c2eba8f56dd4c52344ff
Sam Fentress <sfentress@concord.org>
2012-10-26 13:06:54 -0400

    Add forEach() for browser (ahem IE...) that don't implement it Conflicts:
    	app/javascript/views/function-generator-view.js

--- client-breadboard-activity.js
+++ client-breadboard-activity.js
@@ -446,6 +446,14 @@ replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
 
     sparks.config.root_dir = '/sparks-content';
 
+    if ( !Array.prototype.forEach ) {
+      Array.prototype.forEach = function(fn, scope) {
+        for(var i = 0, len = this.length; i < len; ++i) {
+          fn.call(scope, this[i], i, this);
+        }
+      }
+    }
+
 
 
     sparks.extend = function(Child, Parent, properties) {
