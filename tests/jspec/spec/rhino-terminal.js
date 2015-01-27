423c6e073540dd077b9a033e5478547727788abd
Sam Fentress <sfentress@concord.org>
2015-01-26 22:31:26 -0500

    Clean up folders from gh-pages branch

--- tests/jspec/spec/rhino-terminal.js
+++
@@ -1,5 +0,0 @@
-load('spec/rhino-common.js');
-
-specResults
-.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
-.report();
--- tests/jspec/test.sh
+++
@@ -1,2 +0,0 @@
-bundle exec rake
-java -jar spec/support/js.jar -opt -1 spec/rhino-terminal.js
